c5cb2b43fd2f0e1cb207f185710db0026f174de6
mishaask <122132689+mishaask@users.noreply.github.com>
2025-11-18 11:23:06 +0200

    Refactor InputMover for improved readability

--- Assets/Scripts/1-movers/InputMover.cs
+++ Assets/Scripts/1-movers/InputMover.cs
@@ -1,33 +1,41 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 /**
  * This component moves its object when the player clicks the arrow keys.
  */
-public class InputMover: MonoBehaviour {
+public class InputMover : MonoBehaviour
+{
     [Tooltip("Speed of movement, in meters per second")]
-    [SerializeField] float speed = 10f;
+    [SerializeField]
+    private float speed = 10f;
 
-    [SerializeField] InputAction move = new InputAction(
-        type: InputActionType.Value, expectedControlType: nameof(Vector2));
+    [SerializeField]
+    private InputAction move = new InputAction(
+        type: InputActionType.Value,
+        expectedControlType: nameof(Vector2));
 
-    void OnEnable()  {
+    private void OnEnable()
+    {
         move.Enable();
     }
 
-    void OnDisable()  {
+    private void OnDisable()
+    {
         move.Disable();
     }
 
-    void Update() {
+    private void Update()
+    {
         Vector2 moveDirection = move.ReadValue<Vector2>();
-        Vector3 movementVector = new Vector3(moveDirection.x, moveDirection.y, 0) * speed * Time.deltaTime;
+        Vector3 movementVector =
+            new Vector3(moveDirection.x, moveDirection.y, 0) * speed * Time.deltaTime;
+
         transform.position += movementVector;
-        //transform.Translate(movementVector);
-        // NOTE: "Translate(movementVector)" uses relative coordinates - 
+
+        // NOTE: "Translate(movementVector)" uses relative coordinates -
         //       it moves the object in the coordinate system of the object itself.
-        // In contrast, "transform.position += movementVector" would use absolute coordinates -
-        //       it moves the object in the coordinate system of the world.
+        // In contrast, "transform.position += movementVector" uses world coordinates.
         // It makes a difference only if the object is rotated.
     }
 }
