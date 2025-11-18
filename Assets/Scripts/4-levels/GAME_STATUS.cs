4c814d3b5e857ea34b18708697b3281bcbdd7186
mishaask <122132689+mishaask@users.noreply.github.com>
2025-11-18 11:21:00 +0200

    Update GAME_STATUS.cs

--- Assets/Scripts/4-levels/GAME_STATUS.cs
+++ Assets/Scripts/4-levels/GAME_STATUS.cs
@@ -1,8 +1,9 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 /**
  * This is a static class that keeps the player scores between scenes.
  */
-public class GAME_STATUS: MonoBehaviour {
+public class GAME_STATUS : MonoBehaviour
+{
     public static int playerScore = 0;
 }
