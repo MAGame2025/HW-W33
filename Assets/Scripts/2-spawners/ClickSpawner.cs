fa002ca2f86022299237c567ef3f6f75ea1e77d0
mishaask <122132689+mishaask@users.noreply.github.com>
2025-11-18 11:11:57 +0200

    Refactor ClickSpawner class for improved readability

--- Assets/Scripts/2-spawners/ClickSpawner.cs
+++ Assets/Scripts/2-spawners/ClickSpawner.cs
@@ -1,41 +1,54 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 /**
  * This component spawns the given object whenever the player clicks a given key.
  */
-public class ClickSpawner: MonoBehaviour {
-    [SerializeField] protected InputAction spawnAction = new InputAction(type: InputActionType.Button);
-    [SerializeField] protected GameObject prefabToSpawn;
-    [SerializeField] protected Vector3 velocityOfSpawnedObject;
+public class ClickSpawner : MonoBehaviour
+{
+    [SerializeField] 
+    protected InputAction spawnAction = new InputAction(type: InputActionType.Button);
 
-    void OnEnable()  {
+    [SerializeField] 
+    protected GameObject prefabToSpawn;
+
+    [SerializeField] 
+    protected Vector3 velocityOfSpawnedObject;
+
+    private void OnEnable()
+    {
         spawnAction.Enable();
     }
 
-    void OnDisable()  {
+    private void OnDisable()
+    {
         spawnAction.Disable();
     }
 
-    protected virtual GameObject spawnObject() {
-        Debug.Log("Spawning a new "+prefabToSpawn.name);
+    protected virtual GameObject spawnObject()
+    {
+        Debug.Log("Spawning a new " + prefabToSpawn.name);
 
         // Step 1: spawn the new object.
-        Vector3 positionOfSpawnedObject = transform.position;  // span at the containing object position.
-        Quaternion rotationOfSpawnedObject = Quaternion.identity;  // no rotation.
-        GameObject newObject = Instantiate(prefabToSpawn, positionOfSpawnedObject, rotationOfSpawnedObject);
+        Vector3 positionOfSpawnedObject = transform.position;
+        Quaternion rotationOfSpawnedObject = Quaternion.identity;
+        GameObject newObject =
+            Instantiate(prefabToSpawn, positionOfSpawnedObject, rotationOfSpawnedObject);
 
         // Step 2: modify the velocity of the new object.
         Mover newObjectMover = newObject.GetComponent<Mover>();
-        if (newObjectMover) {
+        if (newObjectMover != null)
+        {
             newObjectMover.SetVelocity(velocityOfSpawnedObject);
         }
 
         return newObject;
     }
-    
-    private void Update() {
-        if (spawnAction.WasPressedThisFrame()) {
+
+    private void Update()
+    {
+        if (spawnAction.WasPressedThisFrame())
+        {
             spawnObject();
         }
     }
