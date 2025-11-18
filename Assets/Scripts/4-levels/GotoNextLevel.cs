979f368e1301f1bd8185b7a7ec113bc074a9aa5b
mishaask <122132689+mishaask@users.noreply.github.com>
2025-11-18 11:21:28 +0200

    Refactor GotoNextLevel class for clarity and structure

--- Assets/Scripts/4-levels/GotoNextLevel.cs
+++ Assets/Scripts/4-levels/GotoNextLevel.cs
@@ -1,16 +1,26 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GotoNextLevel : MonoBehaviour {
-    [SerializeField] string triggeringTag;
-    [SerializeField] [Tooltip("Name of scene to move to when triggering the given tag")] string sceneName;
-    //[SerializeField] NumberField scoreField;
+public class GotoNextLevel : MonoBehaviour
+{
+    [SerializeField]
+    private string triggeringTag;
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == triggeringTag) {
+    [SerializeField]
+    [Tooltip("Name of scene to move to when triggering the given tag")]
+    private string sceneName;
+
+    // [SerializeField] NumberField scoreField;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == triggeringTag)
+        {
             Debug.Log("Moving " + this + " to zero");
-            this.transform.position = Vector3.zero;
-            SceneManager.LoadScene(sceneName);    // Input can either be a serial number or a name; here we use name.
+            transform.position = Vector3.zero;
+
+            // Input can either be a serial number or a name; here we use name.
+            SceneManager.LoadScene(sceneName);
         }
     }
 }
