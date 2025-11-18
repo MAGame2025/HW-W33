9c560f51a7cb74e7ccb80817697de56be69edf31
mishaask <122132689+mishaask@users.noreply.github.com>
2025-11-18 11:10:10 +0200

    Refactor OnTriggerEnter2D method formatting

--- Assets/Scripts/GameOverOnTrigger2D.cs
+++ Assets/Scripts/GameOverOnTrigger2D.cs
@@ -7,19 +7,22 @@ public class GameOverOnTrigger2D : MonoBehaviour
     [Tooltip("Every object tagged with this tag will trigger game over")]
     [SerializeField] string triggeringTag;
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == triggeringTag && enabled) {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == triggeringTag && enabled)
+        {
             Debug.Log("Game over!");
+
 #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;  // Error on editor 2021.3
+            UnityEditor.EditorApplication.isPlaying = false;  // Stop play mode in Editor
 #else
             Application.Quit();
 #endif
         }
     }
 
-    private void Update() {
-        /* Just to show the enabled checkbox in Editor */
+    private void Update()
+    {
+        // Just to show the enabled checkbox in the Editor
     }
-
 }
